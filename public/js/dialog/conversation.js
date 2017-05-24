9015ed28ff3c0eb46f90e7d0196a9e4849396355
Marcelo Gondim <mgmarques94@gmail.com>
2017-05-24 14:37:33 -0300

    com comversation atualizado

--- public/js/dialog/conversation.js
+++ public/js/dialog/conversation.js
@@ -1,26 +1,7 @@
 var http = new XMLHttpRequest();
 var texto = document.getElementById("input");
-
-//teste
+var speech = 'teste';
 var isShow = true;
-
-var iniciar = {
-    input: 'comecar_conversar'
-}
-
-jsonStringify = JSON.stringify(iniciar);
-
-send(jsonStringify);
-
-http.onreadystatechange = function() {
-    if (http.readyState == XMLHttpRequest.DONE) {
-        $(".ms-body").append('<div class="message-feed media"><div class="pull-left"><img src="img/watson.png" alt="" class="img-avatar"></div><div class="media-body"><div class="mf-content">' + http.responseText + '</div><small class="mf-date"><i class="fa fa-clock-o"></i> ' + Date() + '</small></div></div>');
-        scroll();
-        //para o watson falar pode ser encontrado no textToSpeech
-        sendTextToSpeech(http.responseText);
-        //alert(http.responseText);
-    }
-}
 
 function enviarMensagem() {
 
@@ -36,41 +17,58 @@ function enviarMensagem() {
         console.log(jsonStringify);
 
         send(jsonStringify);
+
+        scroll();
+
     } else {
-      alert("é nescessario escrever alguma coisa para iniciar uma conversa com o watson")
+        alert("é nescessario escrever alguma coisa para iniciar uma conversa com o watson")
     }
 }
 
 function handle(e) {
+
     if (e.keyCode === 13) {
         enviarMensagem();
-        if (event.preventDefault) event.preventDefault();
-        return false; // evita a da espaco no input
+
+		e.preventDefault();
+
     }
 }
 
 function send(json) {
 
-    http.open('POST', '/api/message', true);
+    http.open('POST', '/api/message', false);
     http.setRequestHeader('Content-type', 'application/json');
     http.send(json);
 
     obj = JSON.parse(json);
 
-    if (obj.input != 'comecar_conversar') {
-        $(".ms-body").append('<div class="message-feed right"><div class="pull-right"><img src="img/user.png" alt="" class="img-avatar"></div><div class="media-body"><div class="mf-content">' + obj.input + '</div><small class="mf-date"><i class="fa fa-clock-o"></i> ' + Date() + '</small></div></div>');
-        scroll();
-        texto.value = '';
-    }
+    $(".ms-body").append('<div class="message-feed right"><div class="pull-right"><img src="img/user.png" alt="" class="img-avatar"></div><div class="media-body"><div class="mf-content">' + obj.input + '</div><small class="mf-date"><i class="fa fa-clock-o"></i> ' + Date() + '</small></div></div>');
+    scroll();
+    texto.value = '';
+
+    conversation_response();
+
+    sendTextToSpeech(speech);
 
 }
 
-function button_animation(){
-    if(isShow){
-      isShow = false;
-      $('#animation').attr('class','animated bounceOutRight');
+function conversation_response(){
+  var obj = JSON.parse(http.responseText);
+  speech = "";
+  obj.forEach(function(element) {
+     $(".ms-body").append('<div class="message-feed media"><div class="pull-left"><img src="img/watson.png" alt="" class="img-avatar"></div><div class="media-body"><div class="mf-content">' + element + '</div><small class="mf-date"><i class="fa fa-clock-o"></i> ' + Date() + '</small></div></div>');
+     speech += element + " ";
+  }, this);
+
+}
+
+function button_animation() {
+    if (isShow) {
+        isShow = false;
+        $('#animation').attr('class', 'animated bounceOutRight');
     } else {
-      isShow = true;
-      $('#animation').attr('class','animated bounceInRight');
+        isShow = true;
+        $('#animation').attr('class', 'animated bounceInRight');
     }
 }
